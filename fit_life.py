8cfff6572e6cca64b0eb9198e282841cfc614742
Иван Шилкин <ivan_shilkin@bk.ru>
2026-08-13 13:03:02 +0600

    Добавление переменной error. Добавление циклов while True и их улучшение вместе с конструкцией try... except... .

--- fit_life.py
+++ fit_life.py
@@ -1,25 +1,27 @@
 WATER_PER_KG = 30
 IN_LITERS = 1000
 line = '-' * 40
+error = 'Ошибка! Вы ввели некорректное число. Попробуйте ещё раз.'
 print('Привет! Я твой помощник, расскажи немного о себе.')
 user_name = input('Как тебя зовут: ')
 while True:
     try:
         user_age = int(input('Сколько тебе лет: '))
+        break #ИИ подсказал куда лучше всего поставить break
     except ValueError:
-        print('Ошибка! Вы ввели некорректное число. Попробуйте ещё раз.')
-        user_age = int(input('Сколько тебе лет: '))
+        print(error)
+while True:
     try:
         user_weight = float(input('Твой вес в кг(например, 65.5): '))
+        break
     except ValueError:
-        print('Ошибка! Вы ввели некорректное число. Попробуйте ещё раз.')
-        user_weight = float(input('Твой вес в кг(например, 65.5): '))
+        print(error)
+while True:
     try:
         user_height = float(input('Твой рост в метрах(например, 1.75): '))
+        break
     except ValueError:
-        print('Ошибка! Вы ввели некорректное число. Попробуйте ещё раз.')
-        user_height = float(input('Твой рост в метрах(например, 1.75): '))
-    break
+        print(error)
 bmi = user_weight / (user_height ** 2)  # расчёт индекса массы тела
 # расчёт рекомендуемой нормы воды в день и перевод из мл в л
 water_l = user_weight * WATER_PER_KG / IN_LITERS
